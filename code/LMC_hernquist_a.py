55cc710a20ebf480780116f1274b5687be7cd812
jngaravitoc <nico.garavito@gmail.com>
2016-05-15 17:25:18 -0700

    added octopus library and arranged as library

--- code/LMC_hernquist_a.py
+++ code/LMC_hernquist_a.py
@@ -10,23 +10,22 @@ Input:
 Output:
      a: Hernquist scale length.
 """
-
 import numpy as np
-from profiles import mass_hernquist as mh
+from octopus import profiles as pfls
 import sys
 
-M = float(sys.argv[1])
-M_ideal = float(sys.argv[2])
+#M = float(sys.argv[1])
+#M_ideal = float(sys.argv[2])
 
-def a_value(M):
+def a_value(M, M_ideal):
+    # possible a candidates.
     a_values = np.linspace(3, 40, 2000)
     best_a = []
+    # r_obs is where the rotation curve peaks.
+    r_obs = 8.7 # kpc
 
     for i in a_values:
-        Mass_enclosed = mh(i, 9, M)
-        Mass_enclosed = Mass_enclosed
+        Mass_enclosed = pfls.mass_hernquist(i, r_obs, M) #Enclosed mass within r_obs
         if (abs(Mass_enclosed-M_ideal) < 5E8):
             best_a = i
     return best_a
-
-print a_value(M)
