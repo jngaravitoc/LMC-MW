cbb5b3c75cc2ba66f883742968cee8e93172c5ed
jngaravitoc <nico.garavito@gmail.com>
2016-09-05 09:31:52 -0700

    draft PCA

--- code/SCF/covariance_matrix.c
+++ code/SCF/covariance_matrix.c
@@ -1,3 +1,17 @@
+/*
+Author: J. Nicolas Garavito-Camargo
+08/19/2016
+University of Arizona.
+
+Code to compute the covariance matrix of the
+coefficients of the basis expansions functions.
+
+usage:
+
+the code reads an input file
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -6,69 +20,162 @@
 #include <gsl/gsl_sf_gegenbauer.h>
 #include <gsl/gsl_sf_legendre.h>
 
+double Anl_tilde(int n, int l);
+double phi_nl_f(double r, int n, int l);
+double phi_nlm_f(double r, double theta, int n, int l, int m);
+double sum_angular(int n_points, double *r, double *theta, double *phi, double *M, int n, int l, int m);
+void read_data(char *filename, int n_points, double *r, double *theta, double *phi, double *m, double r_s);
+void cov_matrix(int n_points, double *r , double *theta , double *phi, double *m, int max, int lmax);
 
-double Anl_tilde(int(n),int(l)){
+
+void coefficients(int n_points, double *r , double *theta , double *phi, double *m, int max, int lmax);
+
+int main(void){
+     double *r=NULL;
+     double *theta=NULL;
+     double *phi=NULL;
+     double *M=NULL;
+
+     /* Global variables */
+     /* To do: put this as input parameters */
+
+     int n_points=425584;
+     int nmax=10;
+     int lmax=1;
+
+     char filename[20]="spherical_halo.txt";
+     double r_s = 40.85;
+
+     // ------------------------
+
+     printf("%s \n", filename);
+
+     /* Allocating memory for pointers */
+     r = malloc(n_points*sizeof(long double));
+     theta = malloc(n_points*sizeof(long double));
+     phi = malloc(n_points*sizeof(long double));
+     M = malloc(n_points*sizeof(long double));
+
+     read_data(filename, n_points, r, theta, phi, M, r_s);
+     cov_matrix(n_points, r, theta, phi, M, nmax, lmax);;
+     return 0;
+}
+
+double Anl_tilde(int n ,int l){
     double K_nl, factor, A_nl;
     double gamma_factor;
 
     K_nl = 0.5*n*(n+4.*l+3.) + (l+1.)*(2.*l+1.);
     factor = pow(2,8.*l+6.) / (4.*M_PI*K_nl);
     gamma_factor = pow(gsl_sf_gamma(2.0*l+1.5),2)/gsl_sf_gamma(n+4.*l+3.);
-    A_nl =-factor* gsl_sf_fact(n)*(n+2.*l+1.5)*gamma_factor/(4.0*M_PI*K_nl);
-    //printf("%f \n", A_nl);
+    A_nl =-factor* gsl_sf_fact(n)*(n+2.*l+1.5)*gamma_factor;
     return A_nl;
 }
 
-double phi_nl_f(double(r), int(n), int(l)){
+double phi_nl_f(double r, int n, int l){
     double factor, s, C_n;
-    factor = pow(r, l)*pow((1+r), (-2*l-1))*pow(4*M_PI, 0.5);
+    factor = pow(r, l) * pow((1.+r), (-2.*l-1.)) * pow(4*M_PI, 0.5);
     s = (r-1)/(r+1);
-    C_n = gsl_sf_gegenpoly_n(n,2.*l+0.5, s);
-    //printf("%f \n", C_n);
+    C_n = gsl_sf_gegenpoly_n(n,2*l+1.5, s);
     return -factor*C_n;
 }
 
-double phi_nlm_f(double(r), double(theta),int(n), int(l), int(m)){
+/* Function that computes the potential see Eq. in Lowing*/
+double phi_nlm_f(double r, double theta ,int n, int l, int m){
     double Y_lm, phi_nl;
     Y_lm = gsl_sf_legendre_sphPlm(l, m, cos(theta));
     phi_nl = phi_nl_f(r, n, l);
-    //printf("%f \n", Y_lm);
     return phi_nl*Y_lm;
 }
 
-void cov_matrix(double(x), double(y), double(z), double(M), \
-               int(nmax), int(lmax), double(r_s)){
+/* function that sums the angular terms over all the particles */
+double sum_angular(int n_points, double *r, double *theta, double *phi, double *M, int n, int l, int m){
+    double all_angular=0;
+    double phi_nlm;
+    int i;
+    for(i=0;i<=n_points;i++){
+    phi_nlm = phi_nlm_f(r[i], theta[i], n, l, m);
+    //printf("%f \n", phi_nlm);
+    all_angular += phi_nlm*cos(m*phi[i])*M[i];
+    }
+    return all_angular;
+}
+
+void cov_matrix(int n_points, double *r , double *theta , double *phi, double *M, int nmax, int lmax){
 
     int n, l, m, dm0;
-    double r, theta, phi;
-    double phi_nlm, A_nl;
+    double A_nl;
     double All_angular;
-    r = pow(pow(x, 2) + pow(y, 2) + pow(z,2),0.5);
-    theta = acos(z/(r*r_s));
-    phi = atan2(y, x);
-    //Anl_tilde(2,2);
-    phi_nl_f(10, 2, 2);
+    double S;
     for(n=0;n<=nmax;n++){
         for(l=0;l<=lmax;l++){
             for(m=0;m<=l;m++){
-            //printf("m = %d, l=%d , cos_theta=%f\n", m, l, cos(theta));
-            phi_nlm = phi_nlm_f(r, theta, n, l, m);
-            printf("%f \n", phi_nlm);
+
             if(m==0){
-            dm0=0;
-            }
-            else{
             dm0=1.0;
             }
+            else{
+            dm0=0.0;
+            }
+
             A_nl = Anl_tilde(n,l);
-            All_angular = phi_nlm*M*cos(m*phi);
+            All_angular = sum_angular(n_points, r, theta, phi, M, n, l, m);
             S = (2-dm0)*A_nl*All_angular;
-            printf("%f \n", S);
+
+            printf("%f \t \n", S);
             }
         }
     }
 }
 
-int main(){
-     cov_matrix(10.,10., 10, 1, 1, 1, 1);
+
+void coefficients(int n_points, double *r , double *theta , double *phi, double *M, int nmax, int lmax){
+
+    int n, l, m, dm0;
+    double A_nl;
+    double All_angular;
+    double S;
+    for(n=0;n<=nmax;n++){
+        for(l=0;l<=lmax;l++){
+            for(m=0;m<=l;m++){
+
+            if(m==0){
+            dm0=1.0;
+            }
+            else{
+            dm0=0.0;
+            }
+
+            A_nl = Anl_tilde(n,l);
+            All_angular = sum_angular(n_points, r, theta, phi, M, n, l, m);
+            S = (2-dm0)*A_nl*All_angular;
+
+            //intf("%f \t \n", S);
+            }
+        }
+    }
+}
+void read_data(char *filename, int n_points, double *r, double *theta, \
+               double *phi, double *M, double r_s){
+
+    FILE *in;
+    double X, Y, Z, m;
+    int i;
+
+    in = fopen(filename, "r");
+
+    /* Checking if the file is opening*/
+    if(!in){
+        printf("Problem opening file %s \n", filename);
+        exit(1);
+    }
+
+    for(i=0;i<=n_points;i++){
+        fscanf(in, "%lf %lf %lf %lf \n", &X, &Y, &Z, &m);
+        r[i] = pow(pow(X, 2) + pow(Y, 2) + pow(Z,2),0.5)/r_s;
+        theta[i] = acos(Z/(r[i]*r_s));
+        phi[i] = atan2(Y, X);
+        M[i] = m;
+    }
+    fclose(in);
 }
