5440101c49992d8bcbfcd91527d95c872b3afe72
jngaravitoc <nico.garavito@gmail.com>
2016-10-21 13:38:31 -0700

    ;; rm

--- code/SCF/covariance_matrix.c
+++ code/SCF/covariance_matrix.c
@@ -24,42 +24,43 @@ double Anl_tilde(int n, int l);
 double phi_nl_f(double r, int n, int l);
 double phi_nlm_f(double r, double theta, int n, int l, int m);
 double sum_angular(int n_points, double *r, double *theta, double *phi, double *M, int n, int l, int m);
+double sum_angular_prod(int n_points, double *r, double *theta, double *phi, double *M, int n, int l, int m, int n_prime, int l_prime, int m_prime);
 void read_data(char *filename, int n_points, double *r, double *theta, double *phi, double *m, double r_s);
 void cov_matrix(int n_points, double *r , double *theta , double *phi, double *m, int max, int lmax);
 
 
-void coefficients(int n_points, double *r , double *theta , double *phi, double *m, int max, int lmax);
-
 int main(void){
-     double *r=NULL;
-     double *theta=NULL;
-     double *phi=NULL;
-     double *M=NULL;
+    double *r=NULL;
+    double *theta=NULL;
+    double *phi=NULL;
+    double *M=NULL;
 
-     /* Global variables */
-     /* To do: put this as input parameters */
+    /* Global variables */
+    /* To do: put this as input parameters 
+      n_points should be derived when the data is read
+      nmax and lmax should be input parameters
+      filename should be an input parameter
+      output of the data should be an input parameter
+    */
 
-     int n_points=425584;
-     int nmax=10;
-     int lmax=1;
+    int n_points=856182;
+    int nmax=5;
+    int lmax=5;
+    char filename[40]="LRMWLMC_halo_snap0.txt";
+    double r_s = 40.85;
+    // ------------------------
 
-     char filename[20]="spherical_halo.txt";
-     double r_s = 40.85;
+    /* Allocating memory for pointers */
+    r = malloc(n_points*sizeof(long double));
+    theta = malloc(n_points*sizeof(long double));
+    phi = malloc(n_points*sizeof(long double));
+    M = malloc(n_points*sizeof(long double));
 
-     // ------------------------
+    read_data(filename, n_points, r, theta, phi, M, r_s);
+    cov_matrix(n_points, r, theta, phi, M, nmax, lmax);
+    return 0;
+    }
 
-     printf("%s \n", filename);
-
-     /* Allocating memory for pointers */
-     r = malloc(n_points*sizeof(long double));
-     theta = malloc(n_points*sizeof(long double));
-     phi = malloc(n_points*sizeof(long double));
-     M = malloc(n_points*sizeof(long double));
-
-     read_data(filename, n_points, r, theta, phi, M, r_s);
-     cov_matrix(n_points, r, theta, phi, M, nmax, lmax);;
-     return 0;
-}
 
 double Anl_tilde(int n ,int l){
     double K_nl, factor, A_nl;
@@ -72,6 +73,7 @@ double Anl_tilde(int n ,int l){
     return A_nl;
 }
 
+
 double phi_nl_f(double r, int n, int l){
     double factor, s, C_n;
     factor = pow(r, l) * pow((1.+r), (-2.*l-1.)) * pow(4*M_PI, 0.5);
@@ -80,6 +82,7 @@ double phi_nl_f(double r, int n, int l){
     return -factor*C_n;
 }
 
+
 /* Function that computes the potential see Eq. in Lowing*/
 double phi_nlm_f(double r, double theta ,int n, int l, int m){
     double Y_lm, phi_nl;
@@ -87,6 +90,7 @@ double phi_nlm_f(double r, double theta ,int n, int l, int m){
     phi_nl = phi_nl_f(r, n, l);
     return phi_nl*Y_lm;
 }
+
 
 /* function that sums the angular terms over all the particles */
 double sum_angular(int n_points, double *r, double *theta, double *phi, double *M, int n, int l, int m){
@@ -101,35 +105,78 @@ double sum_angular(int n_points, double *r, double *theta, double *phi, double *
     return all_angular;
 }
 
+
+double sum_angular_prod(int n_points, double *r, double *theta, double *phi, double *M, int n, int l, int m, int n_prime, int l_prime, int m_prime){
+    double all_angular=0;
+    double phi_nlm, phi_nlm_prime;
+    int i;
+    for(i=0;i<=n_points;i++){
+      phi_nlm = phi_nlm_f(r[i], theta[i], n, l, m);
+      phi_nlm_prime = phi_nlm_f(r[i], theta[i], n_prime, l_prime, m_prime);
+      all_angular += phi_nlm*phi_nlm_prime*cos(m*phi[i])*cos(m_prime*phi[i])*M[i]*M[i];
+    }
+    return all_angular;
+}
+
 void cov_matrix(int n_points, double *r , double *theta , double *phi, double *M, int nmax, int lmax){
+    int n, l, m, dm0, n_prime, l_prime, m_prime, dm0_prime;
+    double A_nl, A_nl_prime;
+    double All_phi_nlm, All_phi_nlm_prime, All_phi_nlm_mix;
+    double S, S_prime, SS_prime, S_cov_mat;
 
-    int n, l, m, dm0;
-    double A_nl;
-    double All_angular;
-    double S;
     for(n=0;n<=nmax;n++){
-        for(l=0;l<=lmax;l++){
-            for(m=0;m<=l;m++){
+      for(l=0;l<=lmax;l++){
+        for(m=0;m<=l;m++){
+          for(n_prime=0;n_prime<=nmax;n_prime++){
+            for(l_prime=0;l_prime<=lmax;l_prime++){
+              for(m_prime=0;m_prime<=l_prime;m_prime++){
 
-            if(m==0){
-            dm0=1.0;
-            }
-            else{
-            dm0=0.0;
-            }
+                if(m==0){
+                  dm0=1.0;
+                }
+                else{
+                  dm0=0.0;
+                }
 
-            A_nl = Anl_tilde(n,l);
-            All_angular = sum_angular(n_points, r, theta, phi, M, n, l, m);
-            S = (2-dm0)*A_nl*All_angular;
+                if(m_prime==0){
+                  dm0_prime=1.0;
+                }
+                else{
+                  dm0_prime=0.0;
+                }
 
-            printf("%f \t \n", S);
+                A_nl = Anl_tilde(n,l);
+                A_nl_prime = Anl_tilde(n_prime,l_prime);
+                All_phi_nlm = sum_angular(n_points, r, theta, phi, M, n, l, m);
+                All_phi_nlm_prime = sum_angular(n_points, r, theta, phi, M, n_prime, l_prime, m_prime);
+                All_phi_nlm_mix = sum_angular_prod(n_points, r, theta, phi, M, n, l, m, n_prime, l_prime, m_prime);
+                S = (2-dm0)*A_nl*All_phi_nlm;
+                S_prime = (2-dm0_prime)*A_nl_prime*All_phi_nlm_prime;
+                SS_prime = (2-dm0)*(2-dm0_prime)*A_nl*A_nl_prime*All_phi_nlm_mix;
+                S_cov_mat = SS_prime - S*S_prime/n_points;
+
+                printf("%f \n", S_cov_mat);
+              }
             }
+          }
         }
+      }
     }
 }
 
-
+/*
 void coefficients(int n_points, double *r , double *theta , double *phi, double *M, int nmax, int lmax){
+
+    //
+    //basic function that computes the S_nlm coefficient using the
+    //HO basis function. Following Lowing et al. (2011).
+
+    //TO DO:
+    //------
+    //Implement S as an array/matrix.
+    //Implement T computation.
+
+    //
 
     int n, l, m, dm0;
     double A_nl;
@@ -155,6 +202,8 @@ void coefficients(int n_points, double *r , double *theta , double *phi, double 
         }
     }
 }
+*/
+
 void read_data(char *filename, int n_points, double *r, double *theta, \
                double *phi, double *M, double r_s){
 
