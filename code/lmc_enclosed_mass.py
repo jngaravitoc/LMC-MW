690a011e95b829601af6b3a325e3e1c1caab2374
jngaravitoc <jngaravitoc@email.arizona.edu>
2017-05-23 12:14:58 -0700

    enclosed mass for the LMC

--- code/lmc_enclosed_mass.py
+++ code/lmc_enclosed_mass.py
@@ -1,4 +1,4 @@
- # -*- coding: utf-8 -*-
+ 
 import numpy as np
 import matplotlib.pyplot as plt
 from pygadgetreader import *
@@ -44,7 +44,7 @@ def enclosed_mass(pos, mass):
     
     """
     r = (pos[:,0]**2 + pos[:,1]**2 + pos[:,2]**2)**0.5
-    dr = np.linspace(0.5, 15, 20)
+    dr = np.linspace(0.5, 40, 20)
     cm = np.zeros(len(dr))
     for i in range(len(dr)):
         index = np.where(r<dr[i])
@@ -55,15 +55,14 @@ if __name__ == "__main__":
 
     path = sys.argv[1]
     snap_base = sys.argv[2]
-    orbit = np.loadtxt('LMC_orbit/orbits/LMC6_40Mb0_orbit.txt')
-    # r = np.zeros((30,20))
-    # cm_r = np.zeros((30, 20))
-    for i in range(1, 150, 30):
+    orbit_path = sys.argv[3]
+    orbit = np.loadtxt(orbit_path)
+
+    for i in range(1, 140, 20):
         mwlmc_pos = readsnap(path+snap_base+'{:0>3d}'.format(i), 'pos', 'dm')
         mwlmc_mass = readsnap(path+snap_base+'{:0>3d}'.format(i), 'mass', 'dm')
         mwlmc_pid = readsnap(path+snap_base+'{:0>3d}'.format(i), 'pid', 'dm')
-        time = readhead(path+snap_base+'{:0>3d}'.format(i), 'time')
-        orbit = np.loadtxt('LMC_orbit/orbits/LMC6_40Mb0_orbit.txt')
+        time = readhead(path+snap_base+'{:0>3d}'.format(i), 'time') 
         lmc_pos, lmc_mass = MW_LMC_particles(mwlmc_pos, mwlmc_mass, mwlmc_pid, 37500000)
         lmc_pos_cm = re_center(lmc_pos, orbit[i][6:9])
         r, cm_r = enclosed_mass(lmc_pos_cm, lmc_mass)
@@ -71,4 +70,4 @@ if __name__ == "__main__":
     plt.axhline(1.7E10, ls='--', c='k', alpha=0.5)
     plt.axvline(8.7, ls='--', c='k', alpha=0.5)
     plt.legend()
-    plt.show()
+
